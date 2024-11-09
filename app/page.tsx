c0fc727578260c3ccfa3154c5d89f579fba45342
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2024-11-09 16:06:02 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -46,6 +46,8 @@ const data: {
     };
 
 export default function Home() {
+    const sliceData = data.contents.slice(0, 2);
+
     return (
         <>
             <section className={styles.top}>
@@ -66,7 +68,7 @@ export default function Home() {
             <section className={styles.news}>
                 <h2 className={styles.newsTitlr}>News</h2>
                 <ul>
-                    {data.contents.map((article) => (
+                    {sliceData.map((article) => (
                         <li key={article.id}className={styles.list}>
                             <div className={styles.link}>
                                 <Image
