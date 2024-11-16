bd6122da34b862ddf866ea05d27e85b48152e9da
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2024-11-16 15:29:12 +0900

    commit

--- app/_components/ButtonLink/index.tsx
+++ app/_components/ButtonLink/index.tsx
@@ -1,50 +1,15 @@
 import styles from "./index.module.css";
-import Image from "next/image";
 
-import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
 
 type Props = {
-    news: News[];
+    href: string;
+    children: React.ReactNode;
 };
 
-export default function NewsList({ news }: Props) {
-    if (news.length === 0) {
-        return <p>記事がありません</p>;
-    }
+export default function ButtonLink({ href, children }: Props) {
     return (
-        <ul>
-            {news.map((article)=> (
-                <li key={article.id}className={styles.list}>
-                    <div className={styles.link}>
-                            <Image
-                                className={styles.image}
-                                src="/no-image.png"
-                                alt="No Image"
-
-                                width={1200}
-                                height={630}
-                            />
-                            <dl className={styles.content}>
-                                <dt className={styles.title}>{article.title}</dt>
-                                <dd className={styles.meta}>
-                                    <span className={styles.tag}>
-    {article.category.name}</span>
-                                    <span className={styles.data}>
-                                        <Image
-                                            src="/clock.svg"
-                                            alt=""
-                                            width={16}
-                                            height={16}
-                                            priority
-                                        />
-                                        {article.publishedAt}                                </span>
-                            </dd>
-                        </dl>
-                    </div>
-                </li>
-            ))}
-        </ul>
-
+        <a href={href} className={styles.button}>
+            {children}
+        </a>
     );
 }
