57737f284e02e907c914441dc6ff02c09f37b716
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2024-11-30 15:20:48 +0900

    commit

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -5,14 +5,14 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <a href="/" className={styles.logoLink}>
-                    <Image
-                        src="/logo.svg"
-                        alt="SIMPLE"
-                        className={styles.logo}
-                        width={348}
-                        height={133}
-                        priority
-                    />
+                <Image
+                    src="/logo.svg"
+                    alt="SIMPLE"
+                    className={styles.logo}
+                    width={348}
+                    height={133}
+                    priority
+                />
             </a>
         </header>
     );
