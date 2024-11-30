916c4688cbbbb9851c50f41347728627fb4014e0
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2024-11-30 15:13:34 +0900

    commit

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,5 +1,5 @@
-import styles from "./index.module.css";
 import Image from "next/image";
+import styles from "./index.module.css";
 
 import Category from "../Category";
 import Date from "../Date";
@@ -17,23 +17,23 @@ export default function NewsList({ news }: Props) {
     return (
         <ul>
             {news.map((article)=> (
-                <li key={article.id}className={styles.list}>
+                <li key={article.id} className={styles.list}>
                     <div className={styles.link}>
-                            <Image
-                                className={styles.image}
-                                src="/no-image.png"
-                                alt="No Image"
-
-                                width={1200}
-                                height={630}
-                            />
-                            <dl className={styles.content}>
-                                <dt className={styles.title}>{article.title}</dt>
-                                <dd className={styles.meta}>
-                                    <Category category={article.category} />
-                                    <Date date={article.publishedAt ?? article.createdAt} />
-                                </dd>
-                            </dl>
+                        <Image
+                            className={styles.image}
+                            src="/no-image.png"
+                            alt="No Image"
+                            width={1200}
+                            height={630}
+                        />
+                        <dl className={styles.content}>
+                            <dt className={styles.title}>{article.title}</dt>
+                            <dd className={styles.meta}>
+                                <Category category={article.category} />
+                                <Date date={article.publishedAt ??
+                                    article.createdAt} />
+                            </dd>
+                        </dl>
                     </div>
                 </li>
             ))}
