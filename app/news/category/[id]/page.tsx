523deef7341b7fae892dc00b671b62f110234b28
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2024-12-21 14:50:42 +0900

    commit 7

---
+++ app/news/category/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { getCategoryDetail,getNewsList } from "@/app/_libs/microcms";
+import { notFound }from "next/navigation";
+import NewsList from "@/app/_components/NewsList";
+
+type Props = {  
+    params:{
+        id:string;
+    };
+};
+
+export default async function Page({ params }: Props) {
+    const category =await getCategoryDetail(params.id).catch(notFound);
+
+    const { contents: news } = await getNewsList({
+        filters: `category[equals]${category.id}`,
+    })
+
+    return <NewsList news={news} />;
+}
