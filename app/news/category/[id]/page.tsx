32c6805db9dbf8fba0896ddd6588d6b886bb42f5
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2024-12-21 16:27:58 +0900

    commit p.202

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getCategoryDetail,getNewsList } from "@/app/_libs/microcms";
 import { notFound }from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
+import Category from "@/app/_components/Category";
 
 type Props = {  
     params:{
@@ -11,9 +13,20 @@ type Props = {
 export default async function Page({ params }: Props) {
     const category =await getCategoryDetail(params.id).catch(notFound);
 
-    const { contents: news } = await getNewsList({
+    const { contents: news, totalCount } = await getNewsList({
         filters: `category[equals]${category.id}`,
     })
 
-    return <NewsList news={news} />;
+    return (
+        <>
+            <p>
+                <Category category={category} /> の一覧
+            </p>
+            <NewsList news={news} />
+            <Pagination
+                totalCount={totalCount}
+                basePath={`/news/category/${category.id}`}
+            />
+        </>
+    );
 }
--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { getCategoryDetail, getNewsList }from "@/app/_libs/microcms";
+import { getNewsList }from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 import Pagination from "@/app/_components/Pagination";
 import { NEWS_LIST_LIMIT } from "@/app//_constants";
@@ -7,7 +7,6 @@ import { NEWS_LIST_LIMIT } from "@/app//_constants";
 
 type Props ={
     params:{
-        id: string;
         current: string;
     };
 };
@@ -19,9 +18,7 @@ if (Number.isNaN(current) || current < 1) {
     notFound();
 }
 
-    const category =await getCategoryDetail(params.id).catch(notFound);
     const { contents: news, totalCount } =await getNewsList({
-        filters: `category[equals]${category.id}`,
         limit: NEWS_LIST_LIMIT,
         offset: NEWS_LIST_LIMIT*(current -1),
     });
