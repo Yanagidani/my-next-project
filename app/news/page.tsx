f68220e06e1f20ce67292d0621b125d9e9350d9e
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2025-01-11 13:55:43 +0900

    7shou

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { getCategoryDetail, getNewsList }from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 import Pagination from "@/app/_components/Pagination";
+import SearchField from "@/app/_components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app//_constants";
 
 
@@ -32,6 +33,7 @@ if (Number.isNaN(current) || current < 1) {
 
     return (
         <>
+            <SearchField />
             <NewsList news={news} />
             <Pagination totalCount={ totalCount } current={current} />
         </>
